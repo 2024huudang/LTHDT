092ca166a39eebc36c71e591a31e1e68d5f4b2e6
2024huudang <100086015+2024huudang@users.noreply.github.com>
2022-03-14 00:17:46 +0700

    Create point.java

---
+++ point.java
@@ -0,0 +1,29 @@
+# LTHDT
+Học môn LTHDT
+gói  com.company ;
+
+ lớp  công cộng Điểm {
+    nhân  đôi riêng x;
+    tư nhân  đôi y;
+
+    public  Point ( double  x , double  y ) {
+        cái này . x = x;
+        cái này . y = y;
+    }
+
+    public  double  getX () {
+        trả về x;
+    }
+
+    public  void  setX ( double  x ) {
+        cái này . x = x;
+    }
+
+    public  double  getY () {
+        trả lại y;
+    }
+
+    public  void  setY ( double  y ) {
+        cái này . y = y;
+    }
+}
